58be2a99a468474a01a31898a66fe5e19d3063d6
Maxim Egorov <maxim.v.egorov@gmail.com>
2018-04-08 22:14:12 +0300

    Spring boot application

---
+++ src/main/java/ru/maximvegorov/hellorpmpackage/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package ru.maximvegorov.hellorpmpackage.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloController {
+    @GetMapping("hello")
+    public String hello() {
+        return "Hello RPM package!";
+    }
+}
